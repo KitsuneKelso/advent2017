38ad8c900e2d34ec2585f1be97e86a32643e17e8
Fredrik Gustafsson <Fredrik.Gustafsson@nordnet.se>
2017-12-06 17:57:53 +0100

    add eslint and fix linting problems

---
+++ src/components/Day3/Day3.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Day3 = () => (
+  <div>
+    <h3>Day 3: Spiral Memory</h3>
+  </div>
+);
+
+export default Day3;
---
+++ src/components/Day3/Day3.md
@@ -0,0 +1,5 @@
+```js
+const input = 289326;
+
+<Day3 input={input} />
+```
