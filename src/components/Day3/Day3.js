118690ad466a267af0b3051c9ea9f120c8126551
Fredrik Gustafsson <Fredrik.Gustafsson@nordnet.se>
2017-12-11 15:24:47 +0100

    construct loop that will generate a spiral array, starting from center going, out based on input number

--- src/components/Day3/Day3.js
+++ src/components/Day3/Day3.js
@@ -1,9 +1,91 @@
 import React from 'react';
 
-const Day3 = () => (
-  <div>
-    <h3>Day 3: Spiral Memory</h3>
-  </div>
-);
+class Day3 extends React.Component {
+  generateMatrix = depth => {
+    const matrix = [];
+    for (let i = 0; i < depth; i += 1) {
+      matrix[i] = [];
+      for (let j = 0; j < depth; j += 1) {
+        matrix[i][j] = 0;
+      }
+    }
+    return matrix;
+  };
+
+  spiralArray = (max = 5 * 5) => {
+    const matrix = this.generateMatrix(Math.sqrt(max));
+
+    const sequence = [];
+    for (let i = 1; i <= max; i += 1) {
+      sequence.push(i);
+    }
+
+    const width = Math.sqrt(max);
+    const mid = Math.floor(width / 2);
+
+    matrix[mid][mid] = 1;
+    let x = mid;
+    let y = mid;
+    let depth = mid + 1;
+    let shallow = mid - 1;
+
+    for (let j = 1; j < max; ) {
+      // RIGHT
+      while (y < depth) {
+        if (j >= max) {
+          break;
+        }
+        j += 1;
+        y += 1;
+        matrix[x][y] = j;
+      }
+
+      // UP
+      while (x > shallow) {
+        if (j >= max) {
+          break;
+        }
+        j += 1;
+        x -= 1;
+        matrix[x][y] = j;
+      }
+
+      // LEFT
+      while (y > shallow) {
+        if (j >= max) {
+          break;
+        }
+        j += 1;
+        y -= 1;
+        matrix[x][y] = j;
+      }
+
+      // DOWN
+      while (x < depth) {
+        if (j >= max) {
+          break;
+        }
+        j += 1;
+        x += 1;
+        matrix[x][y] = j;
+      }
+
+      depth += 1;
+      shallow -= 1;
+    }
+
+    console.log(matrix);
+    return matrix;
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Day 3: Spiral Memory</h3>
+        <p>{this.spiralArray()}</p>
+      </div>
+    );
+  }
+}
 
 export default Day3;
