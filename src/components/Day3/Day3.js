723d01d838a48864f54839f095f205cc5800e82b
Fredrik Gustafsson <Fredrik.Gustafsson@nordnet.se>
2017-12-11 16:55:38 +0100

    returns manhattan distance of input number from center of spiral

--- src/components/Day3/Day3.js
+++ src/components/Day3/Day3.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Day3 extends React.Component {
   generateMatrix = depth => {
@@ -12,80 +13,79 @@ class Day3 extends React.Component {
     return matrix;
   };
 
-  spiralArray = (max = 5 * 5) => {
-    const matrix = this.generateMatrix(Math.sqrt(max));
-
-    const sequence = [];
-    for (let i = 1; i <= max; i += 1) {
-      sequence.push(i);
-    }
-
-    const width = Math.sqrt(max);
-    const mid = Math.floor(width / 2);
+  spiralArray = (max = 538 ** 2) => {
+    const endpoint = Math.ceil(Math.sqrt(max));
+    const mid = Math.floor(endpoint / 2);
+    const matrix = this.generateMatrix(endpoint);
 
     matrix[mid][mid] = 1;
     let x = mid;
     let y = mid;
     let depth = mid + 1;
     let shallow = mid - 1;
+    let distance = 0;
+
+    const manhattanDistance = (digit, height, width) => {
+      matrix[height][width] = digit;
+      if (digit === max) {
+        distance = Math.abs(height - mid) + Math.abs(width - mid);
+      }
+    };
+
+    const breaker = iter => iter >= max;
 
     for (let j = 1; j < max; ) {
       // RIGHT
       while (y < depth) {
-        if (j >= max) {
-          break;
-        }
+        if (breaker(j)) break;
         j += 1;
         y += 1;
-        matrix[x][y] = j;
+        manhattanDistance(j, x, y);
       }
 
       // UP
       while (x > shallow) {
-        if (j >= max) {
-          break;
-        }
+        if (breaker(j)) break;
         j += 1;
         x -= 1;
-        matrix[x][y] = j;
+        manhattanDistance(j, x, y);
       }
 
       // LEFT
       while (y > shallow) {
-        if (j >= max) {
-          break;
-        }
+        if (breaker(j)) break;
         j += 1;
         y -= 1;
-        matrix[x][y] = j;
+        manhattanDistance(j, x, y);
       }
 
       // DOWN
       while (x < depth) {
-        if (j >= max) {
-          break;
-        }
+        if (breaker(j)) break;
         j += 1;
         x += 1;
-        matrix[x][y] = j;
+        manhattanDistance(j, x, y);
       }
 
       depth += 1;
       shallow -= 1;
     }
 
-    console.log(matrix);
-    return matrix;
+    return distance;
   };
 
   render() {
     return (
       <div>
         <h3>Day 3: Spiral Memory</h3>
-        <p>{this.spiralArray()}</p>
+        <p>Manhattan Distance: {this.spiralArray(this.props.input)}</p>
       </div>
     );
   }
 }
+
+Day3.propTypes = {
+  input: PropTypes.number.isRequired,
+};
 
 export default Day3;
